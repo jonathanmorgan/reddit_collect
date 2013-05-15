5165e3427c40a18ffadae44a5f1966aad66a76dc
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2013-05-14 20:08:43 -0400

    2013-05-14 - Jonathan Morgan - replaced Foobar with reddit_collect.

--- redditCollector.py
+++ redditCollector.py
@@ -5,7 +5,7 @@ This file is part of http://github.com/jonathanmorgan/reddit_collect.
 
 reddit_collect is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+reddit_collect is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License along with http://github.com/jonathanmorgan/reddit_collect. If not, see http://www.gnu.org/licenses/.
 '''
